46beba9d059f9bd65014e14b24a98f3ea3115d20
Tom Elliott <tom@letsdothis.com>
2021-02-06 08:19:58 +0000

    Initial commit

---
+++ TubeStatus/AttributedText.swift
@@ -0,0 +1,70 @@
+/// Copyright (c) 2021 Razeware LLC
+/// 
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+/// 
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+/// 
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+/// 
+/// This project and source code may use libraries or frameworks that are
+/// released under various Open-Source licenses. Use of those libraries and
+/// frameworks are governed by their own individual licenses.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
+
+import SwiftUI
+
+struct AttributedText: View {
+  @State private var size: CGSize = .zero
+
+  let attributedString: NSAttributedString
+
+  init(_ attributedString: NSAttributedString) {
+    self.attributedString = attributedString
+  }
+
+  var body: some View {
+    AttributedTextRepresentable(attributedString: attributedString, size: $size)
+      .frame(width: size.width, height: size.height)
+  }
+
+  struct AttributedTextRepresentable: UIViewRepresentable {
+    let attributedString: NSAttributedString
+    @Binding var size: CGSize
+
+    func makeUIView(context: Context) -> UILabel {
+      let label = UILabel()
+
+      label.lineBreakMode = .byClipping
+      label.numberOfLines = 0
+
+      return label
+    }
+
+    func updateUIView(_ uiView: UILabel, context: Context) {
+      uiView.attributedText = attributedString
+
+      DispatchQueue.main.async {
+        size = uiView.sizeThatFits(uiView.superview?.bounds.size ?? .zero)
+      }
+    }
+  }
+}
